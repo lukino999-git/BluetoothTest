9c37b1323b011121f3d3ffe8f7ac7f75f4d9c4b3
lukino999 <gianluca.maccioni.uk@gmail.com>
2017-04-19 22:56:25 +0100

    Detecting changes made to Bluetooth adapter
    http://stackoverflow.com/questions/9693755/detecting-state-changes-made-to-the-bluetoothadapter
    
    Registered a BroadcastReceiver

--- app/src/main/java/com/lukino999/fullscreen004/MainActivity.java
+++ app/src/main/java/com/lukino999/fullscreen004/MainActivity.java
@@ -2,7 +2,10 @@ package com.lukino999.fullscreen004;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
@@ -18,33 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
     BluetoothAdapter BA = BluetoothAdapter.getDefaultAdapter();
 
-    public static final int BLUETOOTH_HAS_BEEN_ENABLED = 0;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
+    public static final int BLUETOOTH_HAS_BEEN_ENABLED = 1001;
 
 
-    //
-
-
-    public void buttonGetSizeClick(View view) {
-
-        RelativeLayout layoutToBeMeasured = (RelativeLayout) findViewById(R.id.mainLayout);
-        String logText = "\n" + layoutToBeMeasured.getWidth() + "x" + layoutToBeMeasured.getHeight();
-
-        Log.i("toastText", logText);
-        appendToLog(logText);
-        //Toast.makeText(getApplicationContext(), logText, Toast.LENGTH_SHORT).show();
-
-    }
-
-
-    //from stackoverflow: exec code upon activity finishes...
-    //this override makes so that when the intent of enabling the bluetooth returns a positive result, it then calls the afterBluetoothHasBeenEnabled();
+    // from stackoverflow: exec code upon activity finishes...
+    // This override makes so that when the intent of enabling the bluetooth returns a positive result, it then calls the afterBluetoothHasBeenEnabled();
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if(requestCode == BLUETOOTH_HAS_BEEN_ENABLED){
@@ -64,30 +45,101 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    /*
+    You will want to register a BroadcastReceiver to listen for any changes in the state of the BluetoothAdapter:
+
+    As a private instance variable in your Activity (or in a separate class file... whichever one you prefer):
+     */
+    // The actual code
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        // when STATE_OFF
+                        appendToLog("\nBluetoothAdapter.STATE_OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        // when STATE_TURNING_OFF
+                        appendToLog("\nBluetoothAdapter.STATE_TURNING_OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        // when STATE_ON
+                        appendToLog("\nBluetoothAdapter.STATE_ON");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        // when STATE_TURNING_ON
+                        appendToLog("\nBluetoothAdapter.STATE_TURNING_ON");
+                        break;
+                }
+            }
+        }
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Register for broadcasts on BluetoothAdapter state change
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mReceiver, filter);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    /* ... */
+
+        // Unregister broadcast listeners
+        unregisterReceiver(mReceiver);
+    }
+
+    // --- On click
     public void buttonTestBluetoothClick(View view) {
 
-        Log.i("BA.isEnabled", String.valueOf(BA.isEnabled()));
+        appendToLog("\nClick on: Test Bluetooth");
         if (BA.isEnabled()) {
             //Toast.makeText(getApplicationContext(), "Bluetooth is on", Toast.LENGTH_SHORT).show();
             appendToLog("\nBluetooth is on");
         }   else {
-            appendToLog("\nIntent to turn on bluetooth");
-
-
+            appendToLog("\nIntent to turn bluetooth ON");
             Intent i = new Intent(BA.ACTION_REQUEST_ENABLE);
             startActivityForResult(i, BLUETOOTH_HAS_BEEN_ENABLED);
-
-
-
-
         }
 
+
+    }
+
+    public void buttonGetSizeClick(View view) {
+
+        RelativeLayout layoutToBeMeasured = (RelativeLayout) findViewById(R.id.mainLayout);
+        String logText = "\n" + layoutToBeMeasured.getWidth() + "x" + layoutToBeMeasured.getHeight();
+
+        Log.i("toastText", logText);
+        appendToLog(logText);
+        //Toast.makeText(getApplicationContext(), logText, Toast.LENGTH_SHORT).show();
 
     }
 
     public void buttonTestMultilineTextClick(View view) {
         String textToAppend = "\n" + String.valueOf(Math.random()) + "\n";
         appendToLog(textToAppend);
+    }
+
+    public void buttonTurnBluetoothOffClick(View view) {
+        appendToLog("\nClick on: Turn bluetooth OFF");
+        BA.disable();
     }
 
     private void appendToLog(String textToAppend) {
